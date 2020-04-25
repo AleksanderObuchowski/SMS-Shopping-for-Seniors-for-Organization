107a17c20450c92e5411f4e889dfe1906d9e61f8
Avienir <obuchowskialeksander@gmail.com>
2020-04-26 00:15:44 +0200

    add sms handling

--- zakupy_dla_seniora/messages/forms.py
+++ zakupy_dla_seniora/messages/forms.py
@@ -1,22 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, FloatField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
-from flask_babel import _
 
 
 class AddMessagesForm(FlaskForm):
-    content = StringField(_('Message'), validators=[Length(max=500)])
-    contact_number = StringField(_('Phone number'), validators=[Length(max=12)])
-    location = StringField(_('Location'), validators=[Length(max=100)])
-    status = SelectField(_('Status'), choices=[('Taken'),('Waiting for approval'),('Approved')])
-    submit = SubmitField(_('Submit'))
+    content = TextAreaField('Wiadomosc', validators=[Length(max=500)])
+    contact_number = StringField('Numer telefonu', validators=[Length(max=12)])
+    location = StringField('Lokalizacja', validators=[Length(max=100)])
+    # status = SelectField('Status wiadomosci', choices=[('Taken'),('Waiting for approval'),('Approved')])
+    submit = SubmitField('Dodaj')
 
 
 class EditMessagesForm(FlaskForm):
-    content = StringField(_('Message'), validators=[Length(max=500)])
-    contact_number = StringField(_('Phone number'), validators=[Length(max=12)])
-    location = StringField(_('Location'), validators=[Length(max=100)])
-    longtitude = FloatField(_('Longitude'))
-    latitude = FloatField(_('Latitude'))
-    status = SelectField(_('Status'), choices=[('Taken'), ('Waiting for approval'), ('Approved')])
-    submit = SubmitField(_('Submit'))
+    content = StringField('Wiadomosc', validators=[Length(max=500)])
+    contact_number = StringField('Numer telefonu', validators=[Length(max=12)])
+    location = StringField('Lokalizacja', validators=[Length(max=100)])
+    longitude = FloatField('Dlugosc geograficzna')
+    latitude = FloatField('Szerokosc geograficzna')
+    status = SelectField('Status wiadomosci', choices=[('Taken'),('Waiting for approval'),('Approved')])
+    submit = SubmitField('Zapisz')
--- zakupy_dla_seniora/messages/routes.py
+++ zakupy_dla_seniora/messages/routes.py
@@ -2,31 +2,55 @@ from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask_login import current_user
 from zakupy_dla_seniora.messages.forms import AddMessagesForm
 from zakupy_dla_seniora.messages.models import Messages
-from zakupy_dla_seniora.messages.functions import get_location
+from zakupy_dla_seniora.messages.functions import get_location, conversation
+from zakupy_dla_seniora.config import twilio_sid, twilio_auth_token, twilio_phone
 
-messages = Blueprint('messages', __name__, url_prefix='/<lang_code>')
+from twilio.rest import Client
+
+client = Client(twilio_sid, twilio_auth_token)
+messages = Blueprint('messages', __name__,url_prefix='/<lang_code>')
 
 
-@messages.route('/add_message', methods=['GET', 'POST'])
+@messages.route('/add_message', methods=['GET', 'POST'],)
 def add_message():
     form = AddMessagesForm()
     if request.method == 'POST' and form.validate_on_submit():
         _, lat, lon = get_location(form.location.data, search=False)
-        message = Messages(content=form.content.data, contact_number=form.contact_number.data, location=form.location.data,
-                           latitude=lat, longtitude=lon,status='received',created_by=current_user.id)
+        message = Messages(content=form.content.data, contact_number=form.contact_number.data,
+                           location=form.location.data,
+                           latitude=lat, longitude=lon, status='received', created_by=current_user.id)
         message.save()
         print("Dodano Wiadomość")
-        return render_template('forms/add_message.jinja2', form=form,message='Message created successfully!')
+        return render_template('forms/add_message.jinja2', form=form, message='Message created successfully!')
     else:
         form = AddMessagesForm()
         return render_template('forms/add_message.jinja2', form=form)
 
-@messages.route('/take_message/<id>', methods = ['GET'])
+
+@messages.route('/take_message/<id>', methods=['GET'])
 def take_message(id):
     message = Messages.get_by_id(id)
     message.status = 'taken'
     message.volunteer_id = current_user.id
     message.save()
+    client.messages.create(
+        to=message.contact_number,
+        from_=twilio_phone,
+        body='Someone volunteered to take your order, please give us detailed location'
+    )
+
     return redirect(url_for('users.profile'))
 
 
+@messages.route('/receive_sms', methods=['POST'])
+def receieve_sms():
+    phone_number = request.values['From']
+    content = request.values['Body']
+
+    last_message = Messages.get_by_phone(phone_number)
+    result, status = conversation(last_message=last_message, content=content, phone_number=phone_number)
+    client.messages.create(
+        to=phone_number,
+        from_=twilio_phone,
+        body=result['response']
+    )
