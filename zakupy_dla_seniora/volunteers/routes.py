68ed3951b5d8404ed9f6915e6706116f0d537188
Mateusz Anikiej <mateusz.anikiej@gmail.com>
2020-04-25 21:36:57 +0200

    restored balance in volunteers universe

--- zakupy_dla_seniora/volunteers/routes.py
+++ zakupy_dla_seniora/volunteers/routes.py
@@ -1,49 +1,62 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
 from zakupy_dla_seniora import bcrypt
 from zakupy_dla_seniora.auth.functions import employee_role_required
 from flask_login import current_user, login_required
 from zakupy_dla_seniora.organisations.models import Organisations
 from zakupy_dla_seniora.users.functions import random_password
-from zakupy_dla_seniora.volunteers.forms import AddVolunteerForm
+from zakupy_dla_seniora.volunteers.forms import AddVolunteerForm, EditVolunteerForm
 from zakupy_dla_seniora.volunteers.models import Volunteers
-from flask_babel import _
 
-volunteers = Blueprint('volunteers', __name__, url_prefix='/<lang_code>')
+volunteers = Blueprint('volunteers', __name__)
 
 
 @volunteers.route('/volunteer/new', methods=['GET', 'POST'])
 @employee_role_required
 def add_volunteer():
+    """
+    Decides whether user is superuser or regular employee. Based on that fills Select Field for organisations with
+    possible options. Superuser should be able to add Volunteer to all organisations, where Organisation should
+    be able to add Volunteer only to itself.
+    If function detects POST request and form is valid, then generates random password for the Volunteer and
+    adds the record to database.
+    In other case, if method is GET or form is not valid, renders add_volunteer.jinja2 template again.
+
+    TODO Send password through email to a Volunteer with a link to change it.
+    """
+    if current_user.is_superuser:
+        organisations = [
+            (org_id, name) for org_id, name in Organisations.query.with_entities(
+                Organisations.id, Organisations.name
+            ).all()
+        ]
+    else:
+        organisations = (current_user.organisation_id, Organisations.get_name_by_id(current_user.id))
+
     form = AddVolunteerForm()
+    form.organisation.choices = organisations
+
     if request.method == 'POST' and form.validate_on_submit():
-        if current_user.is_employee:
-            org_id = current_user.organisation_id
-        elif form.organisation.data:
-            org_id = Organisations.get_id_by_name(form.organisation.data)
-            if not org_id:
-                error_message = _("There is no such organisation")
-                return render_template('forms/add_volunteer.jinja2', form=form, message=error_message)
-        else:
-            error_message = __("Give organisation")
-            return render_template('forms/add_volunteer.jinja2', form=form, message=error_message)
         passwd = random_password()
         ph = bcrypt.generate_password_hash(passwd).decode('utf-8')
         vol = Volunteers(first_name=form.first_name.data, last_name=form.last_name.data,
-                         phone=form.phone_number.data, email=form.email.data, org_id=org_id,
+                         phone=form.phone_number.data, email=form.email.data, org_id=form.organisation.data,
                          town=form.town.data, distr=form.district.data, pass_hash=ph,
                          created_by=current_user.id)
         vol.save()
-        print(_("Added volutneer: "), form.first_name, " ", form.last_name, " Hasło: ", passwd)
+        print("Dodano wolontariusza: ", form.first_name, " ", form.last_name, " Hasło: ", passwd)
         return redirect(url_for('volunteers.show_all'))
     return render_template('forms/add_volunteer.jinja2', form=form)
 
 
-# Show volunteers for a given organisation
-# For superuser show all volunteers
 @volunteers.route('/volunteers')
 @employee_role_required
 def show_all():
+    """
+    For hackathon purposes gets all volunteers in database at once. Not very efficient solution for production.
+    TODO Paginate request so it won't take all volunteers at once, rather serve them in "packages"
+
+    :returns All Volunteers for superuser and Organisation specific Volunteers for organisation employee.
+    """
     if current_user.is_employee:
         data = Volunteers.get_all_for_organisation(current_user.organisation_id)
     else:
@@ -51,24 +64,23 @@ def show_all():
     return render_template('show_volunteers.jinja2', data=data)
 
 
-# volunteer can only see his profile
-# employee can see all volunteers belonging to his organization
-# superuser can see all volunteers
-@volunteers.route('/volunteers/<id>')
+@volunteers.route('/volunteer/<volunteer_id>')
 @login_required
-def show(id):
-    if current_user.is_superuser:
-        v = Volunteers.get_one(id)
+def show(volunteer_id):
+    """
+    :param volunteer_id:
+    :return volunteer profile template or access denied template:
+    """
+    v = Volunteers.get_by_id(volunteer_id)
+    if v:
         return render_template('view_volunteer_profile.jinja2', volunteer=v)
-    elif current_user.is_employee:
-        v = Volunteers.get_one(id, current_user.organisation_id)
-        if not v:
-            error_message = _("There is no such volunteer in your organisation.")
-        else:
-            return render_template('view_volunteer_profile.jinja2', volunteer=v)
-    else:
-        v = Volunteers.get_one(id, current_user.organisation_id)
-        if not v:
-            return redirect(url_for('board.view'))
-        else:
-            return render_template('view_volunteer_profile.jinja2', volunteer=v)
+    return render_template('error_pages/access_denied.jinja2')
+
+
+@volunteers.route('/volunteer/<volunteer_id>/edit', methods=["GET", "POST"])
+@login_required
+def edit(volunteer_id):
+    form = EditVolunteerForm()
+    return render_template('forms/edit_volunteer.jinja2', form=form)
+
+# TODO Edit Volunteer, delete Volunteer
