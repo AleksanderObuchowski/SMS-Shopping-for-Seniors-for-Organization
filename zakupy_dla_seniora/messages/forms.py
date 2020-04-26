ec593c747f9dd03852c3e9ff8ea018c72462fddd
Avienir <obuchowskialeksander@gmail.com>
2020-04-26 02:25:51 +0200

    changes BOIZ

--- zakupy_dla_seniora/messages/forms.py
+++ zakupy_dla_seniora/messages/forms.py
@@ -1,21 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, FloatField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
-
+from flask_babel import _
 
 class AddMessagesForm(FlaskForm):
-    content = TextAreaField('Wiadomosc', validators=[Length(max=500)])
-    contact_number = StringField('Numer telefonu', validators=[Length(max=12)])
-    location = StringField('Lokalizacja', validators=[Length(max=100)])
-    # status = SelectField('Status wiadomosci', choices=[('Taken'),('Waiting for approval'),('Approved')])
-    submit = SubmitField('Dodaj')
+    content = TextAreaField(_('Message'), validators=[Length(max=500)])
+    contact_number = StringField('Phone Number', validators=[Length(max=12)])
+    location = StringField(_('Location'), validators=[Length(max=100)])
+    submit = SubmitField(_('Add'))
 
 
 class EditMessagesForm(FlaskForm):
-    content = StringField('Wiadomosc', validators=[Length(max=500)])
-    contact_number = StringField('Numer telefonu', validators=[Length(max=12)])
-    location = StringField('Lokalizacja', validators=[Length(max=100)])
-    longitude = FloatField('Dlugosc geograficzna')
-    latitude = FloatField('Szerokosc geograficzna')
-    status = SelectField('Status wiadomosci', choices=[('Taken'),('Waiting for approval'),('Approved')])
-    submit = SubmitField('Zapisz')
+    content = StringField(_('Message'), validators=[Length(max=500)])
+    contact_number = StringField('Phone Number', validators=[Length(max=12)])
+    location = StringField(_('Location'), validators=[Length(max=100)])
+    longitude = FloatField('Latitude')
+    latitude = FloatField('Longitude')
+    status = SelectField('Status', choices=[(_('Taken')),('Waiting for approval'),(_('Approved'))])
+    submit = SubmitField(_('Save'))
